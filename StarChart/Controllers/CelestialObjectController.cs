cb230090f3d3a4730ccebaf41a63797fff2fbf0b
newmanuevers <seangnewman@msn.com>
2020-10-12 19:05:08 -0700

    Struggled trhough Action for Get

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,5 +18,67 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id:int}", Name ="GetById")]
+        public  IActionResult  GetById(int id)
+        {
+            var result = _context.CelestialObjects.Find(id);
+
+            if(result != null)
+            {
+                result.Satellites = _context.CelestialObjects.Where(c => c.OrbitedObjectId == id).ToList();
+                return Ok(result);
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var result = _context.CelestialObjects.Where(n => n.Name == name);
+                       
+            if (result.Any())
+            {
+                foreach (var r in result)
+                {
+                    r.Satellites = _context.CelestialObjects.Where(c => c.OrbitedObjectId == r.Id).ToList();
+                }
+                return Ok(result);
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = _context.CelestialObjects.ToList();
+
+            if (result.Any())
+            {
+                foreach (var r in result)
+                {
+                    r.Satellites = _context.CelestialObjects.Where(c => c.OrbitedObjectId == r.Id).ToList();
+                }
+                return Ok(result);
+            }
+            else
+            {
+                return NotFound("There are no Celestial Objects");
+            }
+
+        }
+
+
+
+
+
     }
 }
